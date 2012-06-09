63e49a124ef85ff75ba28f19a37244091e5cd9bf
Bobby Powers <bobbypowers@gmail.com>
2012-06-08 22:22:01 -0400

    update copyright year to 2012

--- src/queries.h
+++ src/queries.h
@@ -1,4 +1,4 @@
-// Copyright 2011 Bobby Powers. All rights reserved.
+// Copyright 2012 Bobby Powers. All rights reserved.
 // Use of this source code is governed by the MIT
 // license that can be found in the LICENSE file.
 #ifndef _QUERIES_H_
